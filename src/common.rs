6af5fc552c10a1bd0dffd483a07bf3668dbcad6a
zyx-db <dhirajbomma@gmail.com>
2024-04-14 14:58:18 -0700

    cli parsing and vm

--- src/common.rs
+++ src/common.rs
@@ -1,11 +1,10 @@
-use std::hint::unreachable_unchecked;
-
 use crate::value::{Value, ValueArr};
+use std::hint::unreachable_unchecked;
 
 pub enum OpCode {
     OpReturn,
     OpConstant,
-    ConstantIdx(usize)
+    ConstantIdx(usize),
 }
 
 pub struct Chunk {
@@ -14,63 +13,120 @@ pub struct Chunk {
 }
 
 impl Chunk {
-    pub fn new() -> Self{
-        Chunk{code: Vec::new(), constants: Vec::new()}
+    pub fn new() -> Self {
+        Chunk {
+            code: Vec::new(),
+            constants: Vec::new(),
+        }
     }
 
-    pub fn push_constant(&mut self, v: Value){
+    pub fn push_constant(&mut self, v: Value) {
         self.code.push(OpCode::OpConstant);
         self.add_constant(v);
     }
 
-    pub fn push_op(&mut self, op: OpCode){
+    pub fn push_op(&mut self, op: OpCode) {
         self.code.push(op);
     }
 
-    pub fn add_constant(&mut self, v: Value) -> usize{
+    pub fn add_constant(&mut self, v: Value) -> usize {
         let idx = self.constants.len();
         self.constants.push(v);
         self.code.push(OpCode::ConstantIdx(idx));
         idx
     }
+
+    fn get_constant(&self, offset: u32) -> Value {
+        let idx = {
+            match self.code[(offset + 1) as usize] {
+                OpCode::ConstantIdx(x) => x,
+                _ => unsafe { unreachable_unchecked() },
+            }
+        };
+        self.constants[idx]
+    }
 }
 
-pub fn disassemble(chunk: &Chunk, name: &str){
+pub fn disassemble(chunk: &Chunk, name: &str) {
     println!("===={}====", name);
     let mut offset = 0;
-    for instruction in &chunk.code {
-        use OpCode::*;
-        match instruction{
-            OpReturn => {
-                offset += print_simple_instruction("OP_RETURN", offset);
-            }
-            OpConstant => {
-                offset += print_constant_instruction("OP_CONSTANT", offset, chunk)
-            }
-            ConstantIdx(_) => {}
-            _ => {
-                println!("how tf am i matching a idx");
-                unsafe {unreachable_unchecked()}
-            }
+    for _ in 0..chunk.code.len() - 1 {
+        disassemble_instruction(chunk, &mut offset)
+    }
+}
+
+fn disassemble_instruction(chunk: &Chunk, offset: &mut u32) {
+    use OpCode::*;
+    let instruction = &chunk.code[*offset as usize];
+    match instruction {
+        OpReturn => *offset += print_simple_instruction("OP_RETURN", *offset),
+        OpConstant => *offset += print_constant_instruction("OP_CONSTANT", *offset, chunk),
+        ConstantIdx(_) => {}
+        _ => {
+            println!("how tf am i matching a idx");
+            unsafe { unreachable_unchecked() }
         }
     }
 }
 
-fn print_simple_instruction(name: &str, offset: u32) -> u32{
+fn print_simple_instruction(name: &str, offset: u32) -> u32 {
     println!("{:04} {}", offset, name);
     1
 }
 
-fn print_constant_instruction(name: &str, offset: u32, c: &Chunk) -> u32{
-    let idx = {
-        match c.code[(offset + 1) as usize]{
-            OpCode::ConstantIdx(x) => { x }
-            _ => {
-                println!("wtf am i matching");
-                unsafe {unreachable_unchecked()}
-            }
-        }
-    };
-    println!("{:04} {:10} {:16}", offset, name, c.constants[idx]);
+fn print_constant_instruction(name: &str, offset: u32, c: &Chunk) -> u32 {
+    let value = c.get_constant(offset);
+    println!("{:04} {:10} {:16}", offset, name, value);
     2
+}
+
+#[derive(Debug)]
+pub enum VM_Errors {
+    CompileError,
+    RuntimeError,
+}
+
+pub struct VM {
+    chunk: Option<Chunk>,
+    ip: usize,
+    debug: bool,
+}
+
+impl VM {
+    pub const fn new(debug: bool) -> Self {
+        return VM {
+            chunk: None,
+            ip: 0,
+            debug,
+        };
+    }
+
+    pub fn interpret(&mut self, c: Chunk) -> Result<(), VM_Errors> {
+        self.chunk = Some(c);
+        self.ip = 0;
+        return self.run();
+    }
+
+    fn run(&mut self) -> Result<(), VM_Errors> {
+        loop {
+            let instruction = &(self.chunk.as_ref().unwrap()).code[self.ip];
+            if self.debug {
+                let data = self.chunk.as_ref().unwrap();
+                disassemble_instruction(data, &mut (self.ip as u32))
+            }
+            use OpCode::*;
+            match instruction {
+                OpReturn => {
+                    return Ok(());
+                }
+                OpConstant => {
+                    let data = self.chunk.as_ref().unwrap();
+                    let value = data.get_constant(self.ip as u32);
+                    println!("value {}", value)
+                }
+                _ => {}
+            }
+            self.ip += 1;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 mod common;
 mod value;
 
-use common::{Chunk, disassemble};
+use clap::Parser;
+use common::{disassemble, Chunk, VM};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    debug: bool,
+}
 
 fn main() {
+    let cli = Cli::parse();
+
+    let mut vm = VM::new(cli.debug);
     let mut c: Chunk = Chunk::new();
     c.push_constant(1.2);
     c.push_op(common::OpCode::OpReturn);
-    disassemble(&c, "main");
+    if cli.debug {
+        disassemble(&c, "main");
+        println!("");
+        println!("====execution====");
+    }
+
+    vm.interpret(c).unwrap();
 }
